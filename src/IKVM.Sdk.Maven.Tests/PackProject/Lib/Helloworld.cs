83538fead3b2570067946148770ceff3810920a7
Jerome Haltom <jhaltom@alethic.solutions>
2022-06-24 10:43:46 -0500

    Bump nuget cache.

---
+++ src/IKVM.Sdk.Maven.Tests/PackProject/Lib/Helloworld.cs
@@ -0,0 +1,15 @@
+﻿namespace IKVM.Sdk.Maven.Tests.PackProject.Lib
+{
+
+    public static class Helloworld
+    {
+
+        public static string TestJava(string value)
+        {
+            var n = new org.apache.maven.DefaultMaven();
+            return value;
+        }
+
+    }
+
+}
---
+++ src/IKVM.Sdk.Maven.Tests/PackProjectTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using Buildalyzer;
+using Buildalyzer.Environment;
+
+using FluentAssertions;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IKVM.Sdk.Maven.Tests
+{
+
+    [TestClass]
+    public class PackProjectTests
+    {
+
+        /// <summary>
+        /// Forwards MSBuild events to the test context.
+        /// </summary>
+        class TargetLogger : Logger
+        {
+
+            readonly TestContext context;
+
+            /// <summary>
+            /// Initializes a new instance.
+            /// </summary>
+            /// <param name="context"></param>
+            /// <exception cref="ArgumentNullException"></exception>
+            public TargetLogger(TestContext context)
+            {
+                this.context = context ?? throw new ArgumentNullException(nameof(context));
+            }
+
+            public override void Initialize(IEventSource eventSource)
+            {
+                eventSource.AnyEventRaised += (sender, evt) => context.WriteLine($"{evt.Message}");
+            }
+
+        }
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void Can_pack_test_project()
+        {
+            var properties = File.ReadAllLines("IKVM.Sdk.Maven.Tests.properties").Select(i => i.Split('=', 2)).ToDictionary(i => i[0], i => i[1]);
+
+            var nugetPackageRoot = Path.Combine(Path.GetTempPath(), "IKVM.Sdk.Maven.Tests", "nuget", "packages");
+            if (Directory.Exists(nugetPackageRoot))
+                Directory.Delete(nugetPackageRoot, true);
+            Directory.CreateDirectory(nugetPackageRoot);
+
+            var manager = new AnalyzerManager();
+            var analyzer = manager.GetProject(Path.Combine(@"PackProject", "Lib", "PackProjectExe.csproj"));
+            analyzer.SetGlobalProperty("PackageVersion", properties["PackageVersion"]);
+            analyzer.SetGlobalProperty("RestoreSources", string.Join("%3B", "https://api.nuget.org/v3/index.json", Path.GetFullPath(@"nuget")));
+            analyzer.SetGlobalProperty("RestorePackagesPath", nugetPackageRoot + Path.DirectorySeparatorChar);
+
+            // allow NuGet to locate packages in existing global packages folder if set
+            // else fallback to standard location
+            if (Environment.GetEnvironmentVariable("NUGET_PACKAGES") is string nugetPackagesDir && Directory.Exists(nugetPackagesDir))
+                analyzer.SetGlobalProperty("RestoreAdditionalProjectFallbackFolders", nugetPackagesDir);
+            else
+                analyzer.SetGlobalProperty("RestoreAdditionalProjectFallbackFolders", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".nuget", "packages"));
+
+            analyzer.AddBuildLogger(new TargetLogger(TestContext));
+
+            var options = new EnvironmentOptions();
+            options.DesignTime = false;
+            options.TargetsToBuild.Clear();
+            options.TargetsToBuild.Add("Restore");
+            options.TargetsToBuild.Add("Pack");
+            var results = analyzer.Build(options);
+            results.OverallSuccess.Should().Be(true);
+        }
+
+    }
+
+}
